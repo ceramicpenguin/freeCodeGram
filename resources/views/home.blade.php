e86567d524726334427fbdba0ce7a71957c1a2ef
James <james.h@biffbangpow.com>
2020-03-03 21:56:50 +0000

    Auth

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+       <div class="col-12 col-lg-3">
+           <img alt="freecodecamp's profile picture" class="rounded-circle d-block m-auto" src="https://scontent-lhr8-1.cdninstagram.com/v/t51.2885-19/s150x150/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=scontent-lhr8-1.cdninstagram.com&amp;_nc_ohc=FOChOsqB194AX-nwv_3&amp;oh=a51e0b9f3f1dfa7165ebc6e93a651f7b&amp;oe=5E9A00BA">
+       </div>
+        <div class="col-12 col-lg-9">
+            <h2>freecodecamp</h2>
+            <div class="row">
+                <div class="col-4"><strong>271</strong> posts</div>
+                <div class="col-4"><strong>39.4k</strong> followers</div>
+                <div class="col-4"><strong>234</strong> following</div>
+            </div>
+            <div class="pt-3">
+                <p>freeCodeCamp.org</p>
+                <p>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</p>
+            </div>
+        </div>
+        <div class="col-12 pt-4">
+            <div class="row">
+                <div class="col-12 col-lg-4">
+                    <img class="w-100" src="https://scontent-lht6-1.cdninstagram.com/v/t51.2885-15/e35/c100.0.501.501a/s150x150/89260743_2658694764411607_948672185790002051_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com&_nc_cat=107&_nc_ohc=n9Nz7_LWpu4AX99A_IH&oh=25f9130d9ad9b007b4783d3c4f3cbfb0&oe=5E8F5197" />
+                </div>
+                <div class="col-12 col-lg-4">
+                    <img class="w-100" src="https://scontent-lht6-1.cdninstagram.com/v/t51.2885-15/e35/c100.0.501.501a/s150x150/89260743_2658694764411607_948672185790002051_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com&_nc_cat=107&_nc_ohc=n9Nz7_LWpu4AX99A_IH&oh=25f9130d9ad9b007b4783d3c4f3cbfb0&oe=5E8F5197" />
+                </div>
+                <div class="col-12 col-lg-4">
+                    <img class="w-100" src="https://scontent-lht6-1.cdninstagram.com/v/t51.2885-15/e35/c100.0.501.501a/s150x150/89260743_2658694764411607_948672185790002051_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com&_nc_cat=107&_nc_ohc=n9Nz7_LWpu4AX99A_IH&oh=25f9130d9ad9b007b4783d3c4f3cbfb0&oe=5E8F5197" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
